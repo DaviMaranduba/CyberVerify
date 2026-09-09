d60e782fc321c1da49b08014275b1ea91735e875
Davi Maranduba <DaviMaranduba@users.noreply.github.com>
2026-09-09 13:28:36 -0300

    Check messages for suspicious words

--- analysis.py
+++ analysis.py
@@ -1,4 +1,9 @@
+import rules
+
 def analyze():
-    message = input("Enter the message you want to analyze: \n> ")
+    message = input("\033[0;37mEnter the message you want to analyze: \n>\033[0m ")
+    for word in rules.suspicious_words:
+        if word in message.lower():
+            print(word)
     return message
 
