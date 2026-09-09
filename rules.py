857e96b187f34001255c01b51f6e76da3032db72
Davi Maranduba <DaviMaranduba@users.noreply.github.com>
2026-09-09 13:27:49 -0300

    Add suspicious words to rules

---
+++ rules.py
@@ -0,0 +1,9 @@
+suspicious_words = ["urgent",
+                    "click",
+                    "password",
+                    "prize",
+                    "winner",
+                    "verify",
+                    "account",
+                    "payment",
+                    "pix"]
